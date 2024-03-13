2d9024dd599dd1603507c14652aa806915a510e2
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-03-13 18:49:24 +0700

    [REFACTOR] Optimize method getAllStudentWithCourse

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -1,11 +1,9 @@
 package com.advpro.profiling.tutorial.repository;
 
-import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 /**
  * @author muhammad.khadafi
